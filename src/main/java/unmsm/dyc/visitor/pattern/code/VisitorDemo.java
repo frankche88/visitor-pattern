3102b762068d590c77db352d21ca9647bbe33287
Laboratorio FISI <LaboratorioFISI@UNMSM.pe>
2018-11-17 15:58:44 -0500

    agregar diseño

--- src/main/java/unmsm/dyc/visitor/pattern/code/VisitorDemo.java
+++ src/main/java/unmsm/dyc/visitor/pattern/code/VisitorDemo.java
@@ -6,12 +6,25 @@ import unmsm.dyc.visitor.pattern.code.visitor.AutoPartePrintVisitor;
 
 public class VisitorDemo {
 	
+	private final AutoParte car;
+	
+	VisitorDemo() {
+		car = new Car();
+	}
+	
 	public static void main(final String[] args) {
-        final Car car = new Car();
-
-        car.accept(new AutoPartePrintVisitor());
-        car.accept(new AutoParteDoVisitor());
+        
+    
+		new VisitorDemo().execute();
+		
     }
+	
+	
+	public void execute() {
+		
+		car.accept(new AutoPartePrintVisitor());
+        car.accept(new AutoParteDoVisitor());
+	}
 
 }
 
